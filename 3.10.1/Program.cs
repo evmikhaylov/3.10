04fb5c0c22f6ba001734e9b6b5d4c32d8837e0ed
Михайлов Евгений <jackmihailov@outlook.com>
2023-09-27 14:58:51 +0300

    +

---
+++ 3.10.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._10._1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Введите число:");
+			int number = Int32.Parse(Console.ReadLine());
+			string even = "Вы ввели чётное число";
+			string odd = "Вы ввели нечётное число";
+			if (number%2==0)
+			{
+				Console.WriteLine(even);
+			}
+			else
+			{
+				Console.WriteLine(odd);
+			}
+			//Console.WriteLine((number % 2== 0) ? even : odd);
+			Console.ReadKey();
+		}
+	}
+}
